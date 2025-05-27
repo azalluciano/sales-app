c7b6dc3a6e7d856dbaafc4b6afca5d692ffb08c8
luciano <lazaluciano327@gmail.com>
2025-05-27 16:57:03 +0300

    Creation du composant principal vue

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+}
+</style>
