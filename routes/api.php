ea501dfa41b842fe2f6dc859dbe656ea44c4e028
luciano <lazaluciano327@gmail.com>
2025-05-27 18:35:52 +0300

    Creation de l'arborescence et aussi des fichier component du front

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,22 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SaleController;
 
 
 // Routes pour les utilisateurs
 Route::apiResource('users', UserController::class);
+
+// Routes pour les clients
+Route::apiResource('clients', ClientController::class);
+
+// Routes pour les produits
+Route::apiResource('products', ProductController::class);
+
+// Routes pour les ventes
+Route::apiResource('sales', SaleController::class);
+
+// Route pour le rapport
+Route::get('reports/sales-by-client', [SaleController::class, 'salesByClient']);
